9db65e7701d3e3d807e4c1f1c9e8decb801c09dc
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2020-09-07 20:35:35 +0800

    style: 导航栏下方渐变线条

--- src/components/PageBanner.vue
+++ src/components/PageBanner.vue
@@ -14,6 +14,7 @@ export default {
 .banner {
   $h: 75px;
 
+  position: relative;
   width: 100%;
   height: $h;
   background-color: #041541;
@@ -34,6 +35,16 @@ export default {
     letter-spacing: 0px;
     color: #ffffff;
     text-shadow: 0px 3px 8px rgba(65, 115, 255, 0.82);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 2px;
+    background-image: linear-gradient(90deg, #0b2b55 0%, #137af8 92%, #062d51 100%);
   }
 }
 </style>
