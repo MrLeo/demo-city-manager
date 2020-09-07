f65d70b2832140a4ffe2d133ba56e1d2b2dce73f
xuebin.liu <xuebin.liu@zhaopin.com.cn>
2020-09-07 09:57:44 +0800

    chore: publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,16 @@
-// https://cli.vuejs.org/zh/
-// Vue.js CLI4 Vue.config.js标准配置注解 https://juejin.im/post/6844904125885317133
+/**
+ * 官方文档 {@link https://cli.vuejs.org/zh/}
+ * Vue.js CLI4 Vue.config.js标准配置 {@link https://blog.csdn.net/qq_39045645/article/details/103252533}
+ * vue-cli4 全面配置 {@link vue-cli4 全面配置 https://github.com/staven630/vue-cli4-config}
+ */
 
 // const path = require('path')
 // const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   lintOnSave: true, // process.env.NODE_ENV !== 'production',
+
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
 
   css: {
     loaderOptions: {
